cfbc91fceb6b23ae384321b99b740e8fbc1122d5
Michael Cobuluis <MichaelCobuluis@gmail.com>
2017-05-15 21:52:36 -0500

    Finished product

--- Scripts/Slider.js
+++ Scripts/Slider.js
@@ -27,3 +27,42 @@ function startSlider() {
 	}, 3000)
 	
 }
+
+function prev() {
+	newSlide = sliderInt - 1;
+	showSlide(newSlide);
+}
+
+function next() {
+	newSlide = sliderInt + 1;
+	showSlide(newSlide);
+}
+
+function stopLoop () {
+	window.clearInterval(loop);
+}
+
+function showSlide(Id) {
+	stopLoop();
+	if(Id>count){
+		Id = 1;
+	}else if(Id<1){
+		Id = count;
+	}
+	
+	$("#slider>Img").fadeOut(300);
+	$("#slider>Img#"+Id).fadeIn(300);
+	
+	sliderInt = Id;
+	sliderNext = Id+1;
+	startSlider();
+}
+
+$("#slider>Img").hover(
+	function () {
+		stopLoop();
+	},
+	function(){
+		startSlider();
+	}
+	);
